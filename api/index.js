b3a3bb730a415603d1942f61779690d133cca6ff
Sayyid Khan <sayyidkhan92@hotmail.com>
2026-05-24 22:51:14 +0800

    fix: route all /api requests through single vercel function

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import serverModule from '../server-dist/app.cjs'
 
 const { createApiApp, ensureApiBootstrapped } = serverModule
-
 const app = createApiApp()
 
 export default async function handler(req, res) {
   await ensureApiBootstrapped()
   return app(req, res)
 }
+
